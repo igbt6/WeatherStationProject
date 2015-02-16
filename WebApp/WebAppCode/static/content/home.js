361f6df3f3545e97f816ab287f50bc2e3f4ab03d
lukasz <lukasz.uszko@gmail.com>
2015-02-16 23:52:06 +0100

    jquerry UI first tests carried out

--- WebApp/WebAppCode/static/content/home.js
+++ WebApp/WebAppCode/static/content/home.js
@@ -1,28 +1,39 @@
-$(function(){
- 
-    var tmpl,   // Main template HTML
-    tdata = {};  // JSON data object that feeds the template
- 
-    // Initialise page
-    var initPage = function() {
- 
-        // Load the HTML template
-        $.get("/templates/home.html", function(d){
-            tmpl = d;
-        });
+$(function() {
 
-   
-        // Retrieve the server data and then initialise the page  
-   /*     $.getJSON("/v1/albums.json", function (d) {
-            $.extend(tdata, d.data);
-        });
- */
-        // When AJAX calls are complete parse the template 
-        // replacing mustache tags with vars
-        $(document).ajaxStop(function () {
-            var renderedPage = Mustache.to_html( tmpl, tdata );
-            $("body").html( renderedPage );
-        })    
-    }();
+	var tmpl, // Main template HTML
+	tdata = {}; // JSON data object that feeds the template
+
+	// Initialise page
+	var initPage = function() {
+
+		// Load the HTML template
+		$.get("/templates/home.html", function(d) {
+			tmpl = d;
+		});
+
+		// Retrieve the server data and then initialise the page
+		/*
+		 * $.getJSON("/v1/albums.json", function (d) { $.extend(tdata, d.data);
+		 * });
+		 */
+		// When AJAX calls are complete parse the template
+		// replacing mustache tags with vars
+		$(document).ajaxStop(function() {
+			var renderedPage = Mustache.to_html(tmpl, tdata);
+			$("body").html(renderedPage);
+		})
+	}();
+	
+
 });
+
+
+$(document).ready(function() {
+    $("#dlg_hello").dialog({
+			autoOpen : false
+		});
+		$("#btn_hello").button().click(function() {
+			$("#dlg_hello").dialog("open");
+		});
+  });
 
