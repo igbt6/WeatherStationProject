b7b6167689eb4a9130a9163dea2cef4d5a4ab7de
lukasz <lukasz.uszko@gmail.com>
2015-02-23 23:59:54 +0100

    rendering data tables of all stations with mustache implemented

--- WebApp/WebAppDevelopmentTests/pages/allstations.js
+++ WebApp/WebAppDevelopmentTests/pages/allstations.js
@@ -1,22 +1,29 @@
 $(function(){
- 
-   
-    tdata = {};  // JSON data object that feeds the template
- 
+   console.log("HEREEui");
+        tdata = {};  // JSON data object that feeds the template
+
         // Retrieve the server data and then initialise the page  
-        $.getJSON("/v1/stations.json", function (d) {
-        	console.log(d)
-        	console.log(d.data)
-        	console.log("HERE")
-            $.extend(tdata, d.data);
+        $.getJSON("/v1/stations.json", function (obj) {
+            $.extend(tdata, obj.data);
         });
- 
+
         // When AJAX calls are complete parse the template 
         // replacing mustache tags with vars
         $(document).ajaxStop(function () {
-        	var template =" <td>{{station_name}}</td><td>{{country}}</td><td>{{city}}</td><td>{{date}}</td>";
-            var renderedPage = Mustache.to_html(template, tdata );
-            console.log(tdata);
-            $("#gradeX").html( renderedPage );
+        	var template ="<td>{{name}}</td><td>ACTIVE</td><td>{{country}}</td><td>{{city}}</td><td>{{date}}</td>";
+            console.log(tdata.Stations[0]);
+            
+            var stationsHTML = Mustache.to_html(template, tdata.Stations[0] );
+            console.log(stationsHTML);
+            $(".gradeX").replaceWith(stationsHTML );
+
         });    
+    });
+
+<!--
+$(function() {
+    $('#stations-table').DataTable({
+        responsive: true
+    });
 });
+
